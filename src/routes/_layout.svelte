81bb0998519a79e1917233418f56aa4d04ac7fb1
Rory Duncan <robust.rory@gmail.com>
2020-09-07 18:31:53 -0700

    Add coming soon state

--- src/routes/_layout.svelte
+++ src/routes/_layout.svelte
@@ -1,22 +1,32 @@
 <script>
 	import Nav from '../components/Nav.svelte';
 
-	export let segment;
 </script>
 
 <style>
-	main {
-		position: relative;
-		max-width: 56em;
-		background-color: white;
-		padding: 2em;
-		margin: 0 auto;
-		box-sizing: border-box;
+
+	.coming-soon {
+		display: flex;
+		flex-flow: column wrap;
+		height: 50vh;
+		align-items: center;
+		justify-content: center;
+	}
+
+	h1 {
+		text-transform: uppercase;
+		color: #678;
+		font-size: 1.4rem;
+		font-weight: 200;
+		font-family: "Roboto", sans-serif;
 	}
 </style>
 
-<Nav {segment}/>
+<div class="coming-soon">
+	<h1>Coming Soon</h1>
+</div>
+<!-- <Nav {segment}/>
 
 <main>
 	<slot></slot>
-</main>
+</main> -->
