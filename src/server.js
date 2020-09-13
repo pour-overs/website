86d029272ed3f1406ca739984dd2f9a589112a65
Rory Duncan <robust.rory@gmail.com>
2020-09-12 18:47:46 -0700

    Setup response.json

--- src/server.js
+++ src/server.js
@@ -2,16 +2,18 @@ import sirv from 'sirv';
 import polka from 'polka';
 import compression from 'compression';
 import * as sapper from '@sapper/server';
+import responseJSON from "./middleware/response-json.js"
 
 const { PORT, NODE_ENV } = process.env;
 const dev = NODE_ENV === 'development';
 
-polka() // You can also use Express
-	.use(
-		compression({ threshold: 0 }),
-		sirv('static', { dev }),
-		sapper.middleware()
-	)
-	.listen(PORT, err => {
-		if (err) console.log('error', err);
-	});
+polka()
+  .use(
+    responseJSON(),
+    compression({ threshold: 0 }),
+    sirv('static', { dev }),
+    sapper.middleware()
+  )
+  .listen(PORT, err => {
+    if (err) console.log('error', err);
+  });
