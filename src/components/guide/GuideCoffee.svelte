3ea64547a81878fb9ca3f8604cee3fe01f68636a
Rory Duncan <robust.rory@gmail.com>
2020-09-13 21:42:19 -0700

    add guard for guide.coffee

--- src/components/guide/GuideCoffee.svelte
+++ src/components/guide/GuideCoffee.svelte
@@ -3,17 +3,22 @@
 </script>
 
 <h2>Coffee</h2>
-<ul>
-  <li>
-    <strong>Device:</strong>
-    {guide.coffee.device}
-  </li>
-  <li>
-    <strong>Coffee:</strong>
-    {guide.coffee.amount} grams
-  </li>
-  <li>
-    <strong>Grind level:</strong>
-    {guide.coffee.grind}
-  </li>
-</ul>
+
+{#if guide.coffee}
+  <ul>
+    <li>
+      <strong>Device:</strong>
+      {guide.coffee.device}
+    </li>
+    <li>
+      <strong>Coffee:</strong>
+      {guide.coffee.amount} grams
+    </li>
+    <li>
+      <strong>Grind level:</strong>
+      {guide.coffee.grind}
+    </li>
+  </ul>
+{:else}
+  <p>Coffee details are missing from this guide.</p>
+{/if}
