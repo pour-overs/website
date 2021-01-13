6a50b362f63ffb47e329784615e7661f2986d627
Rory Duncan <robust.rory@gmail.com>
2021-01-13 08:06:41 -0800

    Further configuration for domain change

--- src/services/firebase.js
+++ src/services/firebase.js
@@ -1,5 +1,5 @@
 import * as admin from 'firebase-admin';
-import { firebaseConfig } from "@config/firebase.config";
+import { firebaseConfig } from "@config/firebase.config.js";
 const { databaseURL } = firebaseConfig;
 
 admin.initializeApp({
