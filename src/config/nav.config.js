ef838e60547376c45dedc2bba6e687a3bc80da06
Rory Duncan <robust.rory@gmail.com>
2021-01-15 08:24:48 -0800

    Move Route:  /guides -> /guide

--- src/config/nav.config.js
+++ src/config/nav.config.js
@@ -3,6 +3,6 @@ const navigationLink = (label, segment) => ({ label, segment});
 
 export default [
     navigationLink("About", "about"),
-    navigationLink("Guides", "guides"),
+    navigationLink("Guides", "guide"),
     navigationLink("Wiki", "wiki"),
 ];
